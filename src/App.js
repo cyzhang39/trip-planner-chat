50aba522dfa09494104f0dc206e7cbe1213cea77
TimZhang <czhang35@terpail.umd.edu>
2025-07-07 23:58:44 -0400

    link to github

--- src/App.js
+++ src/App.js
@@ -9,11 +9,15 @@ export default function App() {
   const { token } = useContext(AuthContext);
   const navigate = useNavigate();
   return (
-    <Routes>
-      <Route path="/login" element={token ? <Navigate to="/" /> : <Login onSwitch={() => navigate('/register')} />}/>
-      <Route path="/register" element={token ? <Navigate to="/" /> : <Register onSwitch={() => navigate('/login')} />}/>
-      <Route path="/" element={token ? <Conversation /> : <Navigate to="/login" />}/>
-      <Route path="*" element={<Navigate to={token ? "/" : "/login"} />}/>
-    </Routes>
+    <div className='app-root'>
+      <header className='app-header'><a href='https://github.com/cyzhang39/trip-planner-chat' className='github' title='https://github.com/cyzhang39/trip-planner-chat'>View source or report issue on github</a></header>
+      <Routes>
+        <Route path="/login" element={token ? <Navigate to="/" /> : <Login onSwitch={() => navigate('/register')} />}/>
+        <Route path="/register" element={token ? <Navigate to="/" /> : <Register onSwitch={() => navigate('/login')} />}/>
+        <Route path="/" element={token ? <Conversation /> : <Navigate to="/login" />}/>
+        <Route path="*" element={<Navigate to={token ? "/" : "/login"} />}/>
+      </Routes>
+    </div>
+    
   );
 }
